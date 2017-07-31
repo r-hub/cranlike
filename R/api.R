b5e1d5fb91ca0890d4726d72ef136fd1797e96cf
Gábor Csárdi <csardi.gabor@gmail.com>
2017-07-31 14:40:02 +0100

    package_versions() can list extra columns

--- R/api.R
+++ R/api.R
@@ -2,16 +2,21 @@
 #' List all packages and versions in a CRAN-like repository
 #'
 #' @param dir Path to the repository.
-#' @return Data frame with three columns: `Package`, `Version`, `MD5sum`.
+#' @param xcolumns Extra columns to include in the result.
+#' @return Data frame with at least three columns:
+#'   `Package`, `Version`, `MD5sum`.
 #'
 #' @export
 
-package_versions <- function(dir = ".") {
+package_versions <- function(dir = ".", xcolumns = character()) {
   db_file <- get_db_file(dir)
 
   if (!file.exists(db_file)) stop("Database does not exist")
 
+  cols <- unique(c("Package", "Version", "MD5Sum", xcolumns))
+
   with_db(db_file, {
-    dbGetQuery(db, "SELECT Package, Version, MD5sum FROM packages")
+    q <- paste0("SELECT ", paste(cols, collapse = ","), " FROM packages")
+    dbGetQuery(db, q)
   })
 }
